ae4b9bf61a38fff513d143a29e879379a38f1e65
kiyong21c <87814605+kiyong21c@users.noreply.github.com>
2021-12-19 08:13:06 +0900

    Delete webscrapping_basic directory

--- webscrapping_basic/5_user_agent.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = "http://nadocoding.tistory.com"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67"}
-res = requests.get(url, headers=headers)    # url에 접속할때, "User-Agent"키의 값을 넘겨줌
-res.raise_for_status()  # 문제가 생기면 오류를 내고 끝내버림
-with open("nadocoding.html", "w", encoding="utf8") as f:      # 파일을 만들기
-    f.write(res.text)                                      # 파일 내용 쓰기 : res.text내용
